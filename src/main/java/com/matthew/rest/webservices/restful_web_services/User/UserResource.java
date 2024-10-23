76f718a609f3a26efa151ca1c864a32f5f595567
Glowupmatt <glowupmatt@gmail.com>
2024-10-23 15:06:07 -0700

    Refactor UserDaoService to include save method and update UserResource to handle user creation

--- src/main/java/com/matthew/rest/webservices/restful_web_services/User/UserResource.java
+++ src/main/java/com/matthew/rest/webservices/restful_web_services/User/UserResource.java
@@ -2,11 +2,15 @@ package com.matthew.rest.webservices.restful_web_services.User;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ObjectInputFilter.Status;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -18,8 +22,6 @@ public class UserResource {
         this.service = service;
     }
 
-
-
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return service.findAll();
@@ -30,4 +32,9 @@ public class UserResource {
         return service.findOne(id);
     }
     
+    @PostMapping("/users")
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        service.save(user);
+        return new ResponseEntity<>("User created successfully", HttpStatus.CREATED);
+    }
 }
