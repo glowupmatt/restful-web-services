3ad641032536771851d6f5570ea1b9eaf448edf4
Glowupmatt <glowupmatt@gmail.com>
2024-11-01 10:06:39 -0700

    add error handling for wrong input

--- src/main/java/com/matthew/rest/webservices/restful_web_services/Exception/CustomizedREsponseEntityExceptionHandler.java
+++ src/main/java/com/matthew/rest/webservices/restful_web_services/Exception/CustomizedREsponseEntityExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.matthew.rest.webservices.restful_web_services.Exception;
 
 
+import java.net.http.HttpHeaders;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -13,7 +17,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import com.matthew.rest.webservices.restful_web_services.User.UserNotFoundException;
 
 @ControllerAdvice
-public class CustomizedREsponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
     
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) {
@@ -36,4 +40,16 @@ public class CustomizedREsponseEntityExceptionHandler extends ResponseEntityExce
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
     }
     
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+    HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(
+        LocalDateTime.now(),
+        "Validation failed for " + ex.getBindingResult().getObjectName() + ": " + ex.getErrorCount() + " error(s) - " + ex.getBindingResult().getFieldErrors().stream()
+            .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+            .collect(Collectors.joining(", ")),
+        request.getDescription(false)
+    );
+return new ResponseEntity<Object>(errorDetails, HttpStatus.BAD_REQUEST);
+}
+
 }
--- src/main/java/com/matthew/rest/webservices/restful_web_services/User/UserResource.java
+++ src/main/java/com/matthew/rest/webservices/restful_web_services/User/UserResource.java
@@ -1,19 +1,18 @@
 package com.matthew.rest.webservices.restful_web_services.User;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.io.ObjectInputFilter.Status;
 import java.net.URI;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -50,7 +49,7 @@ public class UserResource {
     }
     
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
